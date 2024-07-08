64a5ced240b7a6a957c6493f165a11c202313b25
91leejs <lgchoco@naver.com>
2024-07-08 17:48:27 +0900

    yujin1

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+    //메뉴 영역
+    $(".main li,.sub_bg").mouseover(function(){
+        $(".sub,.sub_bg").stop().slideDown();
+    })//mouseover
+    $(".main li,.sub_bg").mouseout(function(){
+        $(".sub,.sub_bg").stop().slideUp();
+    })//mouseout
+
+    //이미지 영역
+    $(".fade li").eq(0).siblings().hide();
+    var n = 0; // 0 1 2 
+    setInterval(function(){
+
+        //$(".fade li").eq(n).fadeOut();
+        if(n == 2){
+            n = 0;
+        }else{
+            n ++;
+        }//if
+        //$(".fade li").eq(n).fadeIn();
+        $(".fade li").eq(n).fadeIn().siblings().fadeOut();
+
+        console.log(n);
+    },2500)
+})//jQuery
